cab528e8dbe49211b438a2867f84aa4df654eb3f
AHMED AFANDY <eng.computer1@yahoo.com>
2020-06-05 05:08:26 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,7 +2,7 @@ class Helpers
 
   def self.current_user(session)
    user = User.find(session[:user_id])
-  
+
   end
 
   def self.is_logged_in?(session)
@@ -11,6 +11,5 @@ class Helpers
     end
       return false
   end
-
 
 end
